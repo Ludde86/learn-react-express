1a6cca03f34e79eb48e7b6039243b1c788f95514
Ludde86 <bslbjorn@gmail.com>
2020-03-20 13:11:08 +0100

    installing express

---
+++ index.js
@@ -0,0 +1,11 @@
+// bring in express
+const express = require('express');
+
+// initialize express (so we can use its methods)
+const app = express();
+const port = 3000;
+
+// get request, to url/, gwt res -> "send" the req
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

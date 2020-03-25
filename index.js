c55277927dc041fe6d0aa7cda8c6ce5fbeebfa07
Ludde86 <bslbjorn@gmail.com>
2020-03-25 10:33:50 +0100

    setup complete

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require('express');
 
 // initialize express (so we can use its methods)
 const app = express();
+
+// get request to url/ -> then get res -> "send" the req
+app.get('/', (req, res) => res.send('Hello!'));
+
 const port = 3000;
 
-// get request, to url/, gwt res -> "send" the req
-app.get('/', (req, res) => res.send('Hello World!'));
-
+// listen for connections, and returns a http.server
 app.listen(port, () => console.log(`Connected on port ${port}!`));
